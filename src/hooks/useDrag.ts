01679d42f58bf7f32d3ec83c2db4583e6ee198f0
yourui <857172482@qq.com>
2023-03-06 16:18:03 +0800

    fix useDrag.ts

--- src/hooks/useDrag.ts
+++ src/hooks/useDrag.ts
@@ -1,4 +1,16 @@
-const useDrag = (handle: HTMLElement, target: HTMLElement) => {
+/**
+ * 
+ * @param handle 鼠标按下的元素或者杯拖动的元素 必填
+ * @param target 改变宽度的元素 必填
+ * @param maxWidth 最大宽度 非必填 默认400
+ * @param minWidth 最小宽度 非必填 默认100
+ */
+const useDrag = (
+  handle: HTMLElement,
+  target: HTMLElement,
+  maxWidth: number = 400,
+  minWidth: number = 100
+) => {
   const setWidth = (size: number) => {
     target.style.flex = `0 0 ${size}px`
     target.style.width = `${size}px`
@@ -8,10 +20,10 @@ const useDrag = (handle: HTMLElement, target: HTMLElement) => {
     // console.log(handle, 'handle')
     // console.log(target, 'target')
     // console.log(e.clientX, '===')
-    
-    if (e.clientX > 400) {
+
+    if (e.clientX > maxWidth) {
       setWidth(400)
-    } else if (e.clientX < 100) {
+    } else if (e.clientX < minWidth) {
       setWidth(100)
     } else {
       setWidth(e.clientX)
