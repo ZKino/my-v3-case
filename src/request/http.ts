338a8d4304359e9202b429986f1c946fbca8ad73
yourui <857172482@qq.com>
2023-04-14 17:23:36 +0800

    fix

--- src/request/http.ts
+++ src/request/http.ts
@@ -1,27 +1,8 @@
 import axios from 'axios'
 
-const env = import.meta.env
-console.log(env, 'env')
+const instace = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 10000
+})
 
-// 环境的切换
-if (process.env.NODE_ENV === 'development') {
-  axios.defaults.baseURL = 'https://www.baidu.com'
-} else if (process.env.NODE_ENV == 'debug') {
-  axios.defaults.baseURL = 'https://www.ceshi.com'
-} else if (process.env.NODE_ENV == 'production') {
-  axios.defaults.baseURL = 'https://www.production.com'
-}
-
-const myAxios = (axiosConfig: any) => {
-  const service = axios.create({
-    baseURL: '/apiTest',
-    timeout: 10000,
-    headers: {
-      auth_token: 'fs_15982053044_cc03cca1-dd66-5150-b302-d458e2ab789f',
-    },
-  })
-
-  return service(axiosConfig)
-}
-
-export default myAxios
+instace.interceptors.request.use()
