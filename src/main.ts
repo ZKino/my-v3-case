b44764b2fa04880ee5aae1e8dc5206dcecaae4eb
yourui <857172482@qq.com>
2024-05-29 18:32:22 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,17 @@ import { createPinia } from 'pinia'
 import router from './router'
 
 import App from './App.vue'
+import VxeUITable from 'vxe-table'
 
 import 'reset.css'
 import '@/assets/base.scss'
+import 'vxe-table/lib/style.css'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(createPinia())
+app.use(VxeUITable)
 
 app.mount('#app')
 
