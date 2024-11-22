b10f008daeb6f154e187d07a271842ed6ad3ed37
yourui <857172482@qq.com>
2024-11-22 10:57:22 +0800

    ✨ feat:init

---
+++ src/views/组件封装三板斧(子组件).vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useAttrs, useSlots } from "vue";
+const attrs = useAttrs();
+const slots = useSlots();
+
+console.log(attrs, "attrs");
+console.log(slots, "slots");
+</script>
+
+<template>
+  <div class="my-input">
+    <el-input v-bind="attrs">
+      <template #[slotName]="slotProps" v-for="(slot, slotName) in $slots">
+        <slot :name="slotName" v-bind="slotProps"></slot>
+      </template>
+    </el-input>
+  </div>
+</template>
---
+++ src/views/组件封装三板斧(父组件).vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import MyInput from "./组件封装三板斧(子组件).vue";
+
+const name = ref("Tracy");
+
+const handleInput = () => {
+  console.log("在 Input 值改变时触发", name.value);
+};
+
+const handleBlur = () => {
+  console.log("当选择器的输入框失去焦点时触发", name.value);
+};
+</script>
+
+<template>
+  <MyInput
+    v-model="name"
+    :clearable="true"
+    size="large"
+    placeholder="自定义组件"
+    @input="handleInput"
+    @blur="handleBlur"
+  >
+    <template #prepend>Http://</template>
+    <template #append>.com//</template>
+  </MyInput>
+</template>
